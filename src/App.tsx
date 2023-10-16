d42f12035a5974658a1e61c4a3dd58dfbdfcc937
Garongan <alvindo56@gmail.com>
2023-10-17 01:22:52 +0700

    update final

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,13 @@ import HomePage from "./pages/HomePage";
 import "./App.css";
 import { ThemeProvider } from "./components/theme-provider";
 import { Fragment } from "react";
-import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
     <Fragment>
-      <BrowserRouter basename="/">
-        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
           <HomePage />
         </ThemeProvider>
-      </BrowserRouter>
     </Fragment>
   );
 };
